7332d0e6cab33122a4797c5ed6a4b132c70c9c33
pjablonski123 <36215334+pjablonski123@users.noreply.github.com>
2021-07-29 01:33:40 +0200

    dd

--- scanCodeRepo.sh
+++ scanCodeRepo.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -l
+#!/bin/bash
 
 ./twistcli coderepo scan --address https://$PC_CONSOLE_URL -u $PC_USER -p $PC_PASS ./4scan 
 
@@ -7,13 +7,10 @@ result=$(curl -k -u $PC_USER:$PC_PASS -H 'Content-Type: application/json' "https
 critical=$(echo $result | jq -n '[inputs.critical] | add')
 high=$(echo $result | jq -n '[inputs.high] | add')
 
-if [ $critical = 0 && $high = 0 ]; then
+if [ [$critical=0] && [$high=0] ]; then
    echo "Code Repo scan passed!"
    exit 0
 else
-   echo "Code Repo scan failed! - There are $critical Critical and $high High alerts!"
+   echo "Code Repo scan failed - $critical Critical and $high High alerts!"
    exit 1
 fi
-
-
-#|jq '.[0].pass'
