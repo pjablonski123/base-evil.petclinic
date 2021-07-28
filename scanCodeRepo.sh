25e263bdc4fc6a857434af3d6ce3211b21e72fb2
pjablonski123 <36215334+pjablonski123@users.noreply.github.com>
2021-07-29 00:26:06 +0200

    d11

--- scanCodeRepo.sh
+++ scanCodeRepo.sh
@@ -1,13 +1,11 @@
 #!/bin/bash -l
 
-result=(1 1)
-
 ./twistcli coderepo scan --address https://$PC_CONSOLE_URL -u $PC_USER -p $PC_PASS ./4scan 
-#result=$(curl -k -u $PC_USER:$PC_PASS -H 'Content-Type: application/json' "https://$PC_CONSOLE_URL/api/v1/coderepos-ci?limit=1&reverse=true&sort=scanTime")|jq 'vulnerabilityRiskScore'
-echo result=( $(curl -k -u $PC_USER:$PC_PASS -H 'Content-Type: application/json' "https://$PC_CONSOLE_URL/api/v1/coderepos-ci?limit=1&reverse=true&sort=scanTime")|jq -r '.vulnInfo.vulnerabilityDistribution.[critical, high]' )
 
+result=$(curl -k -u $PC_USER:$PC_PASS -H 'Content-Type: application/json' "https://$PC_CONSOLE_URL/api/v1/coderepos-ci?limit=1&reverse=true&sort=scanTime" | jq '.[0].vulnInfo.vulnerabilityDistribution | {critical,high}')
+sum=$(jq -n '[inputs.critical] | add' <<< $(echo ${result/high/critical}))
 
-if [ "${result[1]}" = 0 ]; then
+if [ $sum = 0 ]; then
    echo "Code Repo scan passed!"
    exit 0
 else
