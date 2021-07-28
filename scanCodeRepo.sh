29a9ac9dd5861a7df150e00244d1f57f0eba6a6e
pjablonski123 <36215334+pjablonski123@users.noreply.github.com>
2021-07-28 16:15:59 +0200

    d7

--- scanCodeRepo.sh
+++ scanCodeRepo.sh
@@ -3,7 +3,7 @@
 ./twistcli coderepo scan --address https://$PC_CONSOLE_URL -u $PC_USER -p $PC_PASS ./4scan 
 result=$(curl -k -u $PC_USER:$PC_PASS -H 'Content-Type: application/json' "https://$PC_CONSOLE_URL/api/v1/coderepos-ci?limit=1&reverse=true&sort=scanTime"|jq '.[0].pass')
 
-if [ "$result" == "true" ] then
+if [ "$result" == "true" ]; then
    echo "Code Repo scan passed!"
    exit 0
 else
