8ee2c35315b3e215cb580c6756658f7368bfcc08
pjablonski123 <36215334+pjablonski123@users.noreply.github.com>
2021-07-28 20:03:51 +0200

    d12

--- scanCodeRepo.sh
+++ scanCodeRepo.sh
@@ -5,14 +5,13 @@
 echo result=$(curl -k -u $PC_USER:$PC_PASS -H 'Content-Type: application/json' "https://$PC_CONSOLE_URL/api/v1/coderepos-ci?limit=1&reverse=true&sort=scanTime")|jq '.vulnInfo.vulnerabilityDistribution'
 
 
-: ''
-if [ "$result" = 0 ]; then
+if [ "$result[0]" = 0 ]; then
    echo "Code Repo scan passed!"
    exit 0
 else
    echo "Code Repo scan failed!"
    exit 1
 fi
-''
+
 
 #|jq '.[0].pass'
